4e865c8c0a694b40369cd99ced4e7eafd4a4da52
Marco Koch <marco.koch@uni-saarland.de>
2020-05-21 21:30:06 +0200

    import itembank

---
+++ src/components/database/mat-card.vue
@@ -0,0 +1,75 @@
+<template>
+  <div class="item-cont">
+    <img :src="Item.src" :alt="'Item '+Item.id">
+    <hr class="vsep">
+    <table>
+        <tbody>
+            <tr>
+                <td class="border-right">Difficulty</td>
+                <td>Part-Whole</td>
+            </tr>
+            <tr>
+                <td class="border-right">{{Item.diff}}</td>
+                <td>{{Item.pwc}}</td>
+            </tr>
+        </tbody>
+    </table>
+    <hr class="vsep">
+    <div class="rule-grid">
+        <img :src="require('@/assets/img/plus.svg')" alt="Rule 1" class="rule-ico">
+        <img :src="require('@/assets/img/minus.svg')" alt="Rule 2" class="rule-ico">
+        <img :src="require('@/assets/img/aktualisierung.svg')" alt="Rule 3" class="rule-ico">
+        <img :src="require('@/assets/img/ueberlappung.svg')" alt="Rule 4" class="rule-ico">
+        <img :src="require('@/assets/img/plus.svg')" alt="Rule 5" class="rule-ico">
+        <img :src="require('@/assets/img/planen.svg')" alt="Rule 6" class="rule-ico">
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+    name: "mat-card",
+    props: {
+        Item: Object,
+    },
+}
+</script>
+
+<style scoped>
+table{
+    width: 100%;
+    border-collapse: collapse;
+}
+
+.border-right{
+    border-right: 1px solid black;
+}
+
+td{
+    text-align: center;
+}
+
+.item-cont{
+    border: 1px solid black;
+    width: 210px;
+    margin: 5px auto;
+    padding: 4%;
+}
+
+.rule-grid{
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr;
+    grid-template-rows: 1fr 1fr;
+    justify-items: center;
+    row-gap: 0.5rem;
+    column-gap: 5%;
+}
+
+.rule-ico{
+    width: 70%;
+}
+
+.vsep{
+    margin: 0.5rem auto;
+}
+</style>
--- src/views/database.vue
+++ src/views/database.vue
@@ -1,14 +1,102 @@
 <template>
-  <h1>work in progress</h1>
+  <div>
+    <div class="responsive" v-for=" Item in Items" :key=Item.id>
+        <ItemCard :Item="Item"/>
+    </div>
+  </div>
 </template>
 
 <script>
+import ItemCard from '@/components/database/mat-card.vue'
+
 export default {
     name : "database",
-
+      data() {
+    return{
+      Items : 
+      [
+        {
+          id : 1,
+          src : require('@/assets/img/blank.png'),
+          diff : ".85",
+          pwc : ".21",
+        },
+        {
+          id : 2,
+          src : require('@/assets/img/blank.png'),
+          diff : ".73",
+          pwc : ".32",
+        },
+        {
+          id : 3,
+          src : require('@/assets/img/blank.png'),
+          diff : ".73",
+          pwc : ".32",
+        },
+        {
+          id : 4,
+          src : require('@/assets/img/blank.png'),
+          diff : ".73",
+          pwc : ".32",
+        },
+        {
+          id : 5,
+          src : require('@/assets/img/blank.png'),
+          diff : ".73",
+          pwc : ".32",
+        },
+        {
+          id : 6,
+          src : require('@/assets/img/blank.png'),
+          diff : ".73",
+          pwc : ".32",
+        },
+      ]
+    }
+  },
+    components : {
+        ItemCard,
+    },
 }
 </script>
 
-<style>
+<style scoped>
+.responsive {
+  padding: 0 6px;
+  float: left;
+  width: 16.66666%;
+}
 
+@media only screen and (max-width: 1580px) {
+  .responsive {
+    width: 19.99999%;
+    margin: 6px 0;
+  }
+}
+
+@media only screen and (max-width: 1280px) {
+  .responsive {
+    width: 24.99999%;
+    margin: 6px 0;
+  }
+}
+
+@media only screen and (max-width: 950px) {
+  .responsive {
+    width: 33.33333%;
+    margin: 6px 0;
+  }
+}
+
+@media only screen and (max-width: 630px) {
+  .responsive {
+    width: 49.99999%; 
+  }
+}
+
+@media only screen and (max-width: 420px) {
+  .responsive {
+    width: 100%;
+  }
+}
 </style>
