e18bc303536daea5feb6d20b66ee335e82eba34f
Marco Koch <marco.koch@uni-saarland.de>
2020-08-26 12:46:52 +0200

    added lti footer

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,23 @@
 <template>
 	<div id="app">
 		<div id="nav">
-			<router-link to="/">Home</router-link>|
-			<router-link to="/builder">Builder</router-link>|
-			<router-link to="/database">Database</router-link>|
+			<router-link to="/">Home</router-link>| <router-link to="/builder">Builder</router-link>| <router-link to="/database">Database</router-link>|
 			<router-link to="/editor">Editor</router-link>
 		</div>
 		<router-view />
+		<footer>
+			<div class="footer-content">
+				<img class="footer-logo" src="https://lets-test.it/assets/LTI_logo.svg" />
+				<span class="footer-text"
+					>The Open Matrices Item Bank and the matrices editor were developed as part of the Let's Test It project. This project aims to to make the advantages of computer based testing more
+					accessible in psychology by providing tests that can be run on any device. All services can be used freely for non-commercial research.</span
+				>
+				<hr />
+				<span><a href="https://lets-test.it">Let’s Test It</a></span>
+				<span>Privacy Policy</span>
+				<span>Contact</span>
+			</div>
+		</footer>
 	</div>
 </template>
 
@@ -14,7 +25,7 @@
 require("@/assets/css/normal.css");
 </script>
 
-<style scoped>
+<style scoped lang="scss">
 #app {
 	font-family: Roboto, sans-serif;
 	-webkit-font-smoothing: antialiased;
@@ -37,6 +48,34 @@ require("@/assets/css/normal.css");
 
 #nav a.router-link-exact-active {
 	color: #42b983;
+}
+
+footer {
+	background-color: #fafafa;
+	widows: 100%;
+	padding: 0 25%;
+	.footer-content {
+		grid-area: content;
+		display: grid;
+		grid-template-rows: repeat(3, auto);
+		grid-template-columns: repeat(4, 1fr);
+		grid-template-areas:
+			"logo text text text"
+			"line line line line"
+			"name link1 link2 link3";
+	}
+	.footer-content .footer-logo {
+		grid-area: logo;
+		align-self: center;
+	}
+	.footer-content .footer-text {
+		grid-area: text;
+		margin-top: 0.4rem;
+	}
+	.footer-content hr {
+		grid-area: line;
+		width: 100%;
+	}
 }
 </style>
 
