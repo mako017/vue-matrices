acb92e13f6432c6420b7a88062943e666e67ee92
Marco Koch <marco.koch@uni-saarland.de>
2021-03-10 18:20:45 +0100

    delete on rightclick

--- src/components/builder/construction.vue
+++ src/components/builder/construction.vue
@@ -1,15 +1,15 @@
 <template>
 	<div class="root">
 		<div class="item-container">
-			<svg viewBox="0 0 100 100" class="element e-item" :class="{ selection: selection == 1 }" id="mat1" ref="mat1" val="1" @click="setSelection" @dblclick="clear" />
-			<svg viewBox="0 0 100 100" class="element e-item" :class="{ selection: selection == 2 }" id="mat2" ref="mat2" val="2" @click="setSelection" @dblclick="clear" />
-			<svg viewBox="0 0 100 100" class="element e-item" :class="{ selection: selection == 3 }" id="mat3" ref="mat3" val="3" @click="setSelection" @dblclick="clear" />
-			<svg viewBox="0 0 100 100" class="element e-item" :class="{ selection: selection == 4 }" id="mat4" ref="mat4" val="4" @click="setSelection" @dblclick="clear" />
-			<svg viewBox="0 0 100 100" class="element e-item" :class="{ selection: selection == 5 }" id="mat5" ref="mat5" val="5" @click="setSelection" @dblclick="clear" />
-			<svg viewBox="0 0 100 100" class="element e-item" :class="{ selection: selection == 6 }" id="mat6" ref="mat6" val="6" @click="setSelection" @dblclick="clear" />
-			<svg viewBox="0 0 100 100" class="element e-item" :class="{ selection: selection == 7 }" id="mat7" ref="mat7" val="7" @click="setSelection" @dblclick="clear" />
-			<svg viewBox="0 0 100 100" class="element e-item" :class="{ selection: selection == 8 }" id="mat8" ref="mat8" val="8" @click="setSelection" @dblclick="clear" />
-			<svg viewBox="0 0 100 100" class="element e-item" :class="{ selection: selection == 9 }" id="mat9" ref="mat9" val="9" @click="setSelection" @dblclick="clear" />
+			<svg viewBox="0 0 100 100" class="element e-item" :class="{ selection: selection == 1 }" id="mat1" ref="mat1" val="1" @click="setSelection" @contextmenu.prevent="clear" />
+			<svg viewBox="0 0 100 100" class="element e-item" :class="{ selection: selection == 2 }" id="mat2" ref="mat2" val="2" @click="setSelection" @contextmenu.prevent="clear" />
+			<svg viewBox="0 0 100 100" class="element e-item" :class="{ selection: selection == 3 }" id="mat3" ref="mat3" val="3" @click="setSelection" @contextmenu.prevent="clear" />
+			<svg viewBox="0 0 100 100" class="element e-item" :class="{ selection: selection == 4 }" id="mat4" ref="mat4" val="4" @click="setSelection" @contextmenu.prevent="clear" />
+			<svg viewBox="0 0 100 100" class="element e-item" :class="{ selection: selection == 5 }" id="mat5" ref="mat5" val="5" @click="setSelection" @contextmenu.prevent="clear" />
+			<svg viewBox="0 0 100 100" class="element e-item" :class="{ selection: selection == 6 }" id="mat6" ref="mat6" val="6" @click="setSelection" @contextmenu.prevent="clear" />
+			<svg viewBox="0 0 100 100" class="element e-item" :class="{ selection: selection == 7 }" id="mat7" ref="mat7" val="7" @click="setSelection" @contextmenu.prevent="clear" />
+			<svg viewBox="0 0 100 100" class="element e-item" :class="{ selection: selection == 8 }" id="mat8" ref="mat8" val="8" @click="setSelection" @contextmenu.prevent="clear" />
+			<svg viewBox="0 0 100 100" class="element e-item" :class="{ selection: selection == 9 }" id="mat9" ref="mat9" val="9" @click="setSelection" @contextmenu.prevent="clear" />
 		</div>
 		<div class="opt-container">
 			<svg viewBox="0 0 100 100" class="element e-opt" id="el0" @click="draw(0)" />
